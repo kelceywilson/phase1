be573fcf4fa3e3f54bb92cc4a0fcfe9c743585ed
Kelcey Wilson <kelceywilson@gmail.com>
2017-09-19 16:10:32 -0700

    contacts updated in response to code review

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 My LG Phase 1 Challenge Solutions
 
 Challenge:            | Solution File:
---------------------- | -------------------------            
+--------------------- | -------------------------
 3 2 1 Contacts        | contacts.js
 Contacts with Testing | contacts-with-testing.js
--- contacts.js
+++ contacts.js
@@ -1,7 +1,5 @@
-let contactStorage = [] // here is where you'll store your contacts
+const contactStorage = []; // here is where you'll store your contacts
 
-// bds: Nice function documentation! Consider using JSDoc (http://usejsdoc.org/)
-// bds: for a standard JS function documentation format
 /*
  * addContact
  *
@@ -16,16 +14,14 @@ let contactStorage = [] // here is where you'll store your contacts
  *  Returns:
  *    undefined
  */
-const addContact = function(firstName, lastName, email) {
-  let newContact = {
-    // bds: Do you have a linter? It's not standard JS to use double-quotes for strings
-    "first_name": firstName,
-    "last_name": lastName,
-    "email": email
-  }
+const addContact = (firstName, lastName, email) => {
+  const newContact = {
+    first_name: firstName,
+    last_name: lastName,
+    email,
+  };
   contactStorage.push(newContact);
-}
-// bds: This function is declared but never used. Bad form.
+};
 
 /*
  * addContacts
@@ -50,16 +46,13 @@ const addContact = function(firstName, lastName, email) {
  *  Returns:
  *    undefined
  */
-const addContacts = function(contacts) {
-  console.log("Loading contact data...");
-  // bds: the following simply recreates the array
-  // bds: it would be simpler to say contactStorage = contacts
-  // bds: but how could you use .map() and addContact() ?
-  for (i = 0; i < contacts.length; i++){
-    contactStorage.push(contacts[i]);
-  }
-  console.log("...Finished loading contact data.");
-}
+const addContacts = (contacts) => {
+  console.log('Loading contact data...');
+  contacts.forEach((contact) => {
+    addContact(contact.first_name, contact.last_name, contact.email);
+  });
+  console.log('...Finished loading contact data.');
+};
 
 /*
  * printContacts
@@ -75,152 +68,156 @@ const addContacts = function(contacts) {
  *  Returns:
  *    undefined
  */
-const printContacts = function() {
+const printContacts = () => {
   // sort contacts in ascending order using array#sort
-
-// bds: again, linter: better to use arrow notation here
-  let sorted = contactStorage.sort(function(a, b){
-    if (a.first_name < b.first_name){
+  const sorted = contactStorage.sort((a, b) => {
+    if (a.first_name < b.first_name) {
       return -1;
     }
-    if (a.first_name > b.first_name){
+    if (a.first_name > b.first_name) {
       return 1;
     }
     return 0;
   });
+
+  // Calculate the lengths of longest name & email
+  // in order to create appropriate header length
+  let namesLength = 0;
+  let emailsLength = 0;
+  sorted.forEach((contact) => {
+    const nameLength = contact.first_name.length + contact.last_name.length;
+    const emailLength = contact.email.length;
+    if (nameLength > namesLength) {
+      namesLength = nameLength + 1;
+    }
+    if (emailLength > emailsLength) {
+      emailsLength = emailLength;
+    }
+  });
+
   // log header
-  console.log("All Contacts:");
-  console.log("|----------------------+--------------------------------|");
-  console.log("| Full Name            | Email Address                  |");
-  console.log("|----------------------+--------------------------------|");
+  console.log(namesLength, emailsLength);
+  const header = `|-${'-'.repeat(namesLength)}-+-${'-'.repeat(emailsLength)}-|`;
+  console.log('All Contacts:');
+  console.log(header);
+  console.log(`| Full Name ${' '.repeat(namesLength - 9)}| Email Address${' '.repeat(emailsLength - 12)}|`);
+  console.log(header);
   // loop through contacts and log each none
-
-  // bds: try using .forEach() instead of for
-  for (i = 0; i < sorted.length; i++){
-    //calculate how many spaces to put after each full name and email address
-
-    // bds: what would happen here if the name was more than 20 characters long?
-    let nameSpaces = 20 - sorted[i].first_name.length - sorted[i].last_name.length;
-    let emailSpaces = 31 - sorted[i].email.length;
-
-    // bds: consider using template literals (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
-    // bds: as they're easier to read than string interpolation
-    console.log("| %s %s%s | %s%s |", sorted[i].first_name, sorted[i].last_name, Array(nameSpaces).join(" "), sorted[i].email, Array(emailSpaces).join(" "));
-  }
-  console.log("|----------------------+--------------------------------|");
-}
+  sorted.forEach((contact) => {
+    // calculate how many spaces to put after each full name and email address
+    const nameSpaces = namesLength - contact.first_name.length - contact.last_name.length;
+    const emailSpaces = emailsLength - contact.email.length;
+    console.log(`| ${contact.first_name} ${contact.last_name}${' '.repeat(nameSpaces)}| ${contact.email} ${' '.repeat(emailSpaces)}|`);
+  });
+  console.log(header);
+};
 
 
-
-// bds: a linter will also clean up these blank lines
-
-
-
-////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 /*          Do not make changes below this line           */
-////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
 addContacts([
   {
-    "first_name": "Tanny",
-    "last_name": "Vibert",
-    "email": "tvibert0@illinois.edu",
+    first_name: 'Tanny',
+    last_name: 'Vibert',
+    email: 'tvibert0@illinois.edu',
   },
   {
-    "first_name": "Tova",
-    "last_name": "Myall",
-    "email": "tmyall1@instagram.com",
+    first_name: 'Tova',
+    last_name: 'Myall',
+    email: 'tmyall1@instagram.com',
   },
   {
-    "first_name": "Engracia",
-    "last_name": "Folger",
-    "email": "efolger2@epa.gov",
+    first_name: 'Engracia',
+    last_name: 'Folger',
+    email: 'efolger2@epa.gov',
   },
   {
-    "first_name": "Conroy",
-    "last_name": "Honsch",
-    "email": "chonsch3@sohu.com",
+    first_name: 'Conroy',
+    last_name: 'Honsch',
+    email: 'chonsch3@sohu.com',
   },
   {
-    "first_name": "Virgina",
-    "last_name": "Cankett",
-    "email": "vcankett4@washington.edu",
+    first_name: 'Virgina',
+    last_name: 'Cankett',
+    email: 'vcankett4@washington.edu',
   },
   {
-    "first_name": "Mateo",
-    "last_name": "Da Costa",
-    "email": "mdacosta5@about.com",
+    first_name: 'Mateo',
+    last_name: 'Da Costa',
+    email: 'mdacosta5@about.com',
   },
   {
-    "first_name": "Ambrose",
-    "last_name": "Scullard",
-    "email": "ascullard6@timesonline.co.uk",
+    first_name: 'Ambrose',
+    last_name: 'Scullard',
+    email: 'ascullard6@timesonline.co.uk',
   },
   {
-    "first_name": "Shaylah",
-    "last_name": "Fairney",
-    "email": "sfairney7@stumbleupon.com",
+    first_name: 'Shaylah',
+    last_name: 'Fairney',
+    email: 'sfairney7@stumbleupon.com',
   },
   {
-    "first_name": "Pier",
-    "last_name": "Waine",
-    "email": "pwaine8@unc.edu",
+    first_name: 'Pier',
+    last_name: 'Waine',
+    email: 'pwaine8@unc.edu',
   },
   {
-    "first_name": "Karita",
-    "last_name": "Bough",
-    "email": "kbough9@angelfire.com",
+    first_name: 'Karita',
+    last_name: 'Bough',
+    email: 'kbough9@angelfire.com',
   },
   {
-    "first_name": "Marguerite",
-    "last_name": "Lafayette",
-    "email": "mlafayettea@bravesites.com",
+    first_name: 'Marguerite',
+    last_name: 'Lafayette',
+    email: 'mlafayettea@bravesites.com',
   },
   {
-    "first_name": "Northrop",
-    "last_name": "Bauchop",
-    "email": "nbauchopb@pagesperso-orange.fr",
+    first_name: 'Northrop',
+    last_name: 'Bauchop',
+    email: 'nbauchopb@pagesperso-orange.fr',
   },
   {
-    "first_name": "Devon",
-    "last_name": "Bocking",
-    "email": "dbockingc@comcast.net",
+    first_name: 'Devon',
+    last_name: 'Bocking',
+    email: 'dbockingc@comcast.net',
   },
   {
-    "first_name": "Willdon",
-    "last_name": "Hedley",
-    "email": "whedleyd@purevolume.com",
+    first_name: 'Willdon',
+    last_name: 'Hedley',
+    email: 'whedleyd@purevolume.com',
   },
   {
-    "first_name": "Charil",
-    "last_name": "Clegg",
-    "email": "cclegge@weibo.com",
+    first_name: 'Charil',
+    last_name: 'Clegg',
+    email: 'cclegge@weibo.com',
   },
   {
-    "first_name": "Nessi",
-    "last_name": "Bywaters",
-    "email": "nbywatersf@shop-pro.jp",
+    first_name: 'Nessi',
+    last_name: 'Bywaters',
+    email: 'nbywatersf@shop-pro.jp',
   },
   {
-    "first_name": "Mercy",
-    "last_name": "Browncey",
-    "email": "mbrownceyg@yelp.com",
+    first_name: 'Mercy',
+    last_name: 'Browncey',
+    email: 'mbrownceyg@yelp.com',
   },
   {
-    "first_name": "Didi",
-    "last_name": "Grose",
-    "email": "dgroseh@google.com.hk",
+    first_name: 'Didi',
+    last_name: 'Grose',
+    email: 'dgroseh@google.com.hk',
   },
   {
-    "first_name": "Niccolo",
-    "last_name": "Spruce",
-    "email": "nsprucei@wordpress.com",
+    first_name: 'Niccolo',
+    last_name: 'Spruce',
+    email: 'nsprucei@wordpress.com',
   },
   {
-    "first_name": "Winston",
-    "last_name": "Hixley",
-    "email": "whixleyj@homestead.com",
+    first_name: 'Winston',
+    last_name: 'Hixley',
+    email: 'whixleyj@homestead.com',
   },
-])
+]);
 
-printContacts()
+printContacts();
